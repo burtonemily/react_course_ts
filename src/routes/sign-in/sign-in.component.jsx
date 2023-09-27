43a917a7c81f9441cb51fde34636655501472b9e
Emily <emily@oxen.io>
2023-09-27 13:24:11 +1000

    Lesson 100 Section 6

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,20 @@
 import {
-  signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(user);
   };
+
   return (
     <div>
       <h1>Sign In</h1>
       <button onClick={logGoogleUser}>Sign in with Google PopUp</button>
+      <SignUpForm />
     </div>
   );
 };
