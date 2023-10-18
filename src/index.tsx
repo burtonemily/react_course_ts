837a5553d67043983428273ebac4318c9208abe2
Emily <emily@oxen.io>
2023-10-18 12:14:41 +1100

    Adds in toggle cart open

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ProductsProvider>
+        <UserProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </UserProvider>
+      </ProductsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
